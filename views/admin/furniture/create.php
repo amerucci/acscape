785cb75c1eaf35604e3e0e5268883ba920cb45d7
GamerBike39 <champidub@gmail.com>
2022-11-25 16:48:24 +0100

    add a button addClue with max 3 clue

--- views/admin/furniture/create.php
+++ views/admin/furniture/create.php
@@ -19,11 +19,14 @@
         <label for="action">Action</label>
         <textarea name="action" id="action" class="form-control"></textarea>
     </div>
-    <!-- clue is a json -->
     <div class="form-group">
         <label for="clue">Indice</label>
         <textarea name="clue" id="clue" class="form-control"></textarea>
+        <!-- button for a new clue -->
+        <button type="button" class="btn btn-primary mt-1" id="addClue">Ajouter un indice</button>
     </div>
+
+
     <div class="form-group">
         <label for="padlock">Verrouillé</label>
         <select name="padlock" id="padlock" class="form-control">
@@ -35,3 +38,19 @@
     <button type="submit" class="btn btn-primary">Créer</button>
 
 </form>
+
+<script>
+    // add a new clue with the button addClue  and a name for the new clue (clue2, clue3, ...) and max 3 clues  and move the button addClue after the last clue without jquery
+    let i = 1;
+    document.getElementById('addClue').addEventListener('click', function () {
+        if (i < 3) {
+            i++;
+            let newClue = document.createElement('textarea');
+            newClue.setAttribute('name', 'clue' + i);
+            newClue.setAttribute('id', 'clue' + i);
+            newClue.setAttribute('class', 'form-control mt-2');
+            document.getElementById('clue').parentNode.insertBefore(newClue, document.getElementById(
+                'addClue'));
+        }
+    });
+</script>
