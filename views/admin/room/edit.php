dc8ae80a74e8a95fe1fcf9918ba40ec884872c00
GamerBike39 <champidub@gmail.com>
2022-11-28 14:13:31 +0100

    script -> room room -> furnitures

--- views/admin/room/edit.php
+++ views/admin/room/edit.php
@@ -26,3 +26,23 @@ $title = "Modifier la salle {$room->title}";
     <input type="hidden" name="script_id" value="<?= $_SESSION['script_id'] ?>">
     <button type="submit" class="btn btn-primary">Editer</button>
 </form>
+
+<?php var_dump($params['room']->id); ?>
+<?php $_SESSION['room_id'] = $params['room']->id; ?>
+<?php var_dump($_SESSION); ?>
+
+<a href="/acscape/admin/furniture/create" class="btn btn-primary">Créer un meuble</a>
+
+<!-- afficher furnitures si room_id = $_SESSION room_id -->
+<?php foreach ($params['furnitures'] as $furniture) : ?>
+<?php if ($furniture->room_id == $_SESSION['room_id']) : ?>
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title"><?= $furniture->title ?></h5>
+        <p class="card-text"><?= $furniture->description ?></p>
+        <a href="/acscape/admin/furniture/edit/<?= $furniture->id ?>" class="btn btn-primary">Editer</a>
+        <a href="/acscape/admin/furniture/delete/<?= $furniture->id ?>" class="btn btn-danger">Supprimer</a>
+    </div>
+</div>
+<?php endif; ?>
+<?php endforeach; ?>
