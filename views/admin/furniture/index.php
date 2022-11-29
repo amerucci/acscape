a1a693a9734f5aef8feadcb86bcb6742a09d2b75
GamerBike39 <champidub@gmail.com>
2022-11-29 12:09:15 +0100

    delete echo

--- views/admin/furniture/index.php
+++ views/admin/furniture/index.php
@@ -1,22 +1,20 @@
 <?php $title = "Administration des meubles"; ?>
 <a href="furniture/create">Créer un meuble</a>
 
-<?php
 
-if (count($params['furnitures']) == 0) {
-    echo "<p>vous n'avez pas encore de meuble</p>";
-} else {
-    foreach ($params['furnitures'] as $furniture) {
-        echo "<div class='card'>";
-        echo "<div class='card-body'>";
-        echo "<h5 class='card-title'>{$furniture->title}</h5>";
-        echo "<p class='card-text'>{$furniture->getExcerpt()}</p>";
-        echo "image : <img src='/acscape/assets/pictures/furnitures/{$furniture->picture}' alt='image du meuble' width='100px' height='100px'>";
-        echo "<a href='/acscape/admin/furniture/show/{$furniture->id}' class='btn btn-primary'>Voir</a>";
-        echo "<a href='/acscape/admin/furniture/edit/{$furniture->id}' class='btn btn-primary mx-3'>editer</a>";
-        echo "</div>";
-        echo "</div>";
-    }
-}
-
-?>
+<?php if (count($params['furnitures']) == 0) : ?>
+<p>vous n'avez pas encore de meuble</p>))
+<?php else : ?>
+<?php foreach ($params['furnitures'] as $furniture) : ?>
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title"><?= $furniture->title ?></h5>
+        <p class="card-text"><?= $furniture->getExcerpt() ?></p>
+        <img src="/acscape/assets/pictures/furnitures/<?= $furniture->picture ?>" alt="image du meuble" width="100px"
+            height="100px">
+        <a href="/acscape/admin/furniture/show/<?= $furniture->id ?>" class="btn btn-primary">Voir</a>
+        <a href="/acscape/admin/furniture/edit/<?= $furniture->id ?>" class="btn btn-primary mx-3">editer</a>
+    </div>
+</div>
+<?php endforeach; ?>
+<?php endif; ?>
--- views/admin/objects/index.php
+++ views/admin/objects/index.php
@@ -1,20 +1,21 @@
 <?php $title = "Administration des objets"; ?>
 <a href="create">Créer un objet</a>
 
-<?php
 
-if (count($params['objects']) == 0) {
-    echo "<p>vous n'avez pas encore d'objet</p>";
-} else {
-    foreach ($params['objects'] as $object) {
-        echo "<div class='card'>";
-        echo "<div class='card-body'>";
-        echo "<h5 class='card-title'>{$object->title}</h5>";
-        echo "<p class='card-text'>{$object->getExcerpt()}</p>";
-        echo "image : <img src='/acscape/assets/pictures/objects/{$object->picture}' alt='image de l'objet' width='100px' height='100px'>";
-        echo "<a href='/acscape/admin/objects/show/{$object->id}' class='btn btn-primary'>Voir</a>";
-        echo "<a href='/acscape/admin/objects/edit/{$object->id}' class='btn btn-primary mx-3'>editer</a>";
-        echo "</div>";
-        echo "</div>";
-    }
-}
+
+<?php if (count($params['objects']) == 0) : ?>
+<p>vous n'avez pas encore d'objet</p>
+<?php else : ?>
+<?php foreach ($params['objects'] as $object) : ?>
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title"><?= $object->title ?></h5>
+        <p class="card-text"><?= $object->getExcerpt() ?></p>
+        <img src="/acscape/assets/pictures/objects/<?= $object->picture ?>" alt="image de l'objet" width="100px"
+            height="100px">
+        <a href="/acscape/admin/objects/show/<?= $object->id ?>" class="btn btn-primary">Voir</a>
+        <a href="/acscape/admin/objects/edit/<?= $object->id ?>" class="btn btn-primary mx-3">editer</a>
+    </div>
+</div>
+<?php endforeach; ?>
+<?php endif; ?>
