11c9a7781a1a29ba4875d42f350e978b1900250f
GamerBike39 <champidub@gmail.com>
2022-12-01 10:25:43 +0100

    homepage in progress

--- views/layout.php
+++ views/layout.php
@@ -5,14 +5,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php if (strpos($_SERVER['REQUEST_URI'], 'login') == false): ?>
+    <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'app.css' ?>">
+    <?php endif ?>
     <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'login.css' ?>">
     <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'navbar.css' ?>">
     <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'footer.css' ?>">
     <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'style.css' ?>">
-    <!-- si l'url contient admin ajouter le ficher css app.css -->
-    <?php if (strpos($_SERVER['REQUEST_URI'], 'admin') !== false): ?>
-    <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'app.css' ?>">
-    <?php endif ?>
+    <link rel="stylesheet" href="<?= SCRIPTS . 'css' . DIRECTORY_SEPARATOR . 'home.css' ?>">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+
     <title>Document</title>
 </head>
 
@@ -32,13 +39,13 @@
         <div>
             <ul class="navLog">
                 <li>
-                    <a alt="" href=""><img src="assets\front\nav\user-avatar.svg"> SE CONNECTER</a>
+                    <a alt="" href="login"><img src="assets\front\nav\user-avatar.svg"> SE CONNECTER</a>
                 </li>
             </ul>
         </div>
     </nav>
 
-    <div class="container">
+    <div class="container-fluid gx-0">
         <?= $content ?>
     </div>
 
