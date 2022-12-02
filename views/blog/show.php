8146a0542d96c61a91bf945a08f5adfc369be48b
GamerBike39 <champidub@gmail.com>
2022-12-02 16:05:33 +0100

    show alignement

--- views/blog/show.php
+++ views/blog/show.php
@@ -6,6 +6,7 @@
     <div class="content_one_game d-flex align-items-center flex-column justify-content-center">
         <div class='d-flex align-items-center justify-content-center  h-100'>
             <div class="description_one_game">
+                <h3 class="my-3 py-3 light">Le développeur perdu</h3>
                 <p> C'est l'histoire d'un étudiant en programmation qui se retrouve perdu dans un labyrinthe de code. Il
                     doit trouver la sortie pour pouvoir sortir de ce labyrinthe et pouvoir finir son projet. Mais
                     attention,
@@ -36,4 +37,77 @@
             </div>
         </div>
     </div>
+
+    <div class="other_games my-5">
+        <h3 class="my-5">Nos autres escape games</h3>
+
+        <div class="d-flex justify-content-center align-items-center gap-5">
+            <div class="game">
+                <img src="assets\front\index\figure.jpg" alt="">
+                <div class="card_game_content">
+                    <div class="title_game">
+                        <p class="m-0">Le Développeur Perdu</p>
+                    </div>
+                    <div class="parameters_game d-flex  align-items-center">
+                        <div class="diffuclty">
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                        </div>
+                        <div class="time_game d-flex justify-content-center align-items-center gap-3">
+                            <img src="assets/front/icons/clock.svg" alt="">
+                            <p class="m-0">60</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="game">
+                <img src="assets\front\index\figure-1.jpg" alt="">
+                <div class="card_game_content">
+                    <div class="title_game">
+                        <p class="m-0">Le Développeur Perdu</p>
+                    </div>
+                    <div class="parameters_game d-flex  align-items-center">
+                        <div class="diffuclty">
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                        </div>
+                        <div class="time_game d-flex justify-content-center align-items-center gap-3">
+                            <img src="assets/front/icons/clock.svg" alt="">
+                            <p class="m-0">60</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="game">
+                <img src="assets\front\index\figure-2.jpg" alt="">
+                <div class="card_game_content">
+                    <div class="title_game">
+                        <p class="m-0">Le Développeur Perdu</p>
+                    </div>
+                    <div class="parameters_game d-flex  align-items-center">
+                        <div class="diffuclty">
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                            <iconify-icon icon="uil:padlock"></iconify-icon>
+                        </div>
+                        <div class="time_game d-flex justify-content-center align-items-center gap-3">
+                            <img src="assets/front/icons/clock.svg" alt="">
+                            <p class="m-0">60</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </div>
