21d9d8f81003c5f0f3836ecc1da77276bef80b6c
GamerBike39 <champidub@gmail.com>
2022-12-22 11:18:47 +0100

    delete font

--- views/layout.php
+++ views/layout.php
@@ -20,10 +20,6 @@
 
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        rel="stylesheet">
     <script src="https://code.iconify.design/iconify-icon/1.0.2/iconify-icon.min.js"></script>
     <title>Document</title>
 </head>
