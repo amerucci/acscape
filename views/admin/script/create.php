a96e81bdd8e8d42509d3a76035050de82784fb68
GamerBike39 <champidub@gmail.com>
2023-01-02 17:14:10 +0100

    update preview img

--- views/admin/script/create.php
+++ views/admin/script/create.php
@@ -32,8 +32,14 @@
             </div>
             <div
                 class="form-group d-flex justify-content-center align-items-center flex-column gap-1 my-5 form_picture w-100">
-                <label for="picture">Image</label>
-                <input type="file" class="form-control-file" name="picture" id="picture">
+                <label for="picture">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="100" height="150" viewBox="0 0 20 17">
+                        <path
+                            d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z">
+                        </path>
+                    </svg>
+                </label>
+                <input type="file" class="form-control-file inputfile" name="picture" id="picture">
                 <img src="" alt="" id="picturePreview">
                 <img src="" alt="" id="picturePreviewTemp">
             </div>
@@ -78,8 +84,8 @@
             // Configurez l'attribut src de l'élément img avec l'URL de l'objet File
             img.src = URL.createObjectURL(file);
             // Configurez l'attribut width et height de l'élément img
-            img.width = 100;
-            img.height = 100;
+            img.width = 400;
+            img.height = 400;
             // Récupérez l'élément parent de la miniature
             const parent = img.parentNode;
             // Ajoutez l'élément img à l'élément parent, ou remplacez-le s'il existe déjà
